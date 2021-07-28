9fd70f8a5b6681dc4028be322f13b426db8bb4bb
samar-chandra <samarc476@gmail.com>
2021-07-28 18:37:30 +0530

    added features

--- js/main.js
+++ js/main.js
@@ -1,37 +1,52 @@
-jQuery(document).ready(function(){
-     
+jQuery(document).ready(function() {
+
     "use strict";
     $(this).scrollTop(0);
-     $('#slider-carousel').carouFredSel({
-         responsive:true,
-         width:'100%',
-         circular:true,
-         scroll:{
-                 items:1,
-                 duration:500,
-                 pauseOnHover:true
-         },
-         auto:true,
-         items:
-         {
-             visible:{
-                   min:1,
-                   max:1
-             },
-             height:"variable"
-         },
-         pagination:{
-             container:".sliderpager",
-             pageAnchorBuilder:false
-         }
-         
-     });
-     $(window).scroll(function(){
-        var top=$(window).scrollTop();
-        if(top>=60){
+    $('#slider-carousel').carouFredSel({
+        responsive: true,
+        width: '100%',
+        circular: true,
+        scroll: {
+            items: 1,
+            duration: 500,
+            pauseOnHover: true
+        },
+        auto: true,
+        items: {
+            visible: {
+                min: 1,
+                max: 1
+            },
+            height: "variable"
+        },
+        pagination: {
+            container: ".sliderpager",
+            pageAnchorBuilder: false
+        }
+
+    });
+    $(window).scroll(function() {
+        var top = $(window).scrollTop();
+        if (top >= 60) {
             $("header").addClass('secondary');
-        }else if($("header").hasClass('secondary')){
+        } else if ($("header").hasClass('secondary')) {
             $("header").removeClass('secondary');
         }
-     });
+    });
+    $('#menu').slicknav({
+        label: '',
+    })
+    $('a').smoothScroll();
+    $('.animate__animated').each(function() {
+
+        var waypoint = new Waypoint({
+            element: this,
+            handler: function(direction) {
+                var animation = $(this.element).attr('data-animation');
+                $(this.element).css('opacity', '1');
+                $(this.element).addClass("animated " + animation);
+            },
+            offset: '75%'
+        })
+    });
 });
